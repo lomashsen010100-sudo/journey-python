b38edce1f59a6044629ed77b9865f6fcbe7dcf03
Lomash <lomashsen010100@gmail.com>
2026-09-05 10:29:42 +0530

    First commit

---
+++ Calendar.py
@@ -0,0 +1,10 @@
+# import calendar as your month and year choice.
+
+import calendar
+
+while True:
+    year=int(input("Enter Year:="))
+    month=int(input("Enter Month:="))
+    
+    cal=calendar.month(year, month)
+    print(cal)
